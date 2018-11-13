3282cb8d479a77c165cee66776af77cc643f955d
Mallika <sundaresh.sankaran@gmail.com>
2018-11-13 16:05:55 -0500

    Burger handlebar homework

---
+++ README.md
@@ -0,0 +1,9 @@
+# Burger App
+
+
+This app will display the selected menu option based on a selection of burgers.
+
+## Deployed: Heroku app at : ___ (TBD)
+## Authors
+
+Sundaresh 
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+/* Insert data */
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Big Mack the Knife and Fork');
+INSERT INTO burgers (burger_name) VALUES ('Filet o Fish');
+INSERT INTO burgers (burger_name) VALUES ('Mutton Madness');
+INSERT INTO burgers (burger_name) VALUES ('Slice of Tomato');
+INSERT INTO burgers (burger_name) VALUES ('Angus Beef');
